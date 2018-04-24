9d7b291db91a75c71867c761f39bedfb7d8f3e81
Samuel Dillow <tarwich@gmail.com>
2018-04-24 12:11:17 -0500

    Added build task to run the old "release"

--- bin/auto.js
+++ bin/auto.js
@@ -6,11 +6,14 @@ switch (argv[2]) {
   case 'release':
     require('../actions/autorelease');
     break;
-  case 'release-notes':
-    require('../actions/release-notes');
+  case 'build':
+    require('../actions/release');
     break;
   case 'check-package':
     require('../actions/check-package');
+    break;
+  case 'release-notes':
+    require('../actions/release-notes');
     break;
   case 'scan-for-import-index':
     require('../actions/scan-for-import-index');
