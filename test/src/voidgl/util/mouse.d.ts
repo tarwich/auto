3b2c1dc31b45e332a612e5092f5c54752699a434
Diniden <diniden@outlook.com>
2018-11-19 21:49:15 -0700

    Removed test files

--- test/src/voidgl/util/mouse.d.ts
+++
@@ -1,7 +0,0 @@
-import { Vector2 } from "three";
-declare let normalizeWheel: (e: MouseWheelEvent) => Vector2;
-declare function eventElementPosition(e: any, relative?: HTMLElement): {
-    x: number;
-    y: number;
-};
-export { eventElementPosition, normalizeWheel };
